06a4cbe316b6eec5d986d155eeeee94dfb4a6821
justinaus <justinaus2k@gmail.com>
2024-12-06 14:57:57 +0900

    Add mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import '@/styles/globals.css';
+import '@mantine/core/styles.css';
 
+import { createTheme, MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
 
+import AppLayout from '@/components/shared/layout/AppLayout';
+
+import { pretendard } from './fonts/pretendard/pretendard';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const theme = createTheme({
+    /** Put your mantine theme override here */
+    fontFamily: 'pretendard, sans-serif',
+  });
+
+  return (
+    <MantineProvider theme={theme}>
+      <AppLayout className={pretendard.className}>
+        <Component {...pageProps} />
+      </AppLayout>
+    </MantineProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { ColorSchemeScript } from '@mantine/core';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Firmunity" />
+        <link rel="icon" href="/favicon.ico" />
+        <ColorSchemeScript defaultColorScheme="auto" />
+      </Head>
       <body>
         <Main />
         <NextScript />

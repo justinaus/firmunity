819abe7d8f380acc6329ed8df58aad6ae83cdc10
justinaus <justinaus2k@gmail.com>
2024-12-13 00:46:05 +0900

    Update next-auth signIn - create user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { Provider } from '@prisma/client';
 import nextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
+import { prismaClient } from '@/helpers/prisma';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -21,6 +24,48 @@ export const authOptions: NextAuthOptions = {
     async session({ session, token }) {
       session.accessToken = token.accessToken as string;
       return session;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async signIn({ user, account, profile }) {
+      if (!user.email || !account) return false;
+
+      try {
+        // 이미 존재하는 유저인지 확인
+        const existingUser = await prismaClient.user.findUnique({
+          where: { email: user.email },
+        });
+
+        if (existingUser) {
+          return true;
+        }
+
+        if (!user.name || !user.email) {
+          throw new Error('signIn error: something wrong');
+        }
+
+        if (account.provider !== 'google') {
+          throw new Error('signIn error: invalid provider');
+        }
+
+        await prismaClient.user.create({
+          data: {
+            email: user.email,
+            name: user.name,
+            image: user.image,
+            account: {
+              create: {
+                provider: Provider.GOOGLE,
+                providerAccountId: account.providerAccountId,
+              },
+            },
+          },
+        });
+
+        return true;
+      } catch (error) {
+        console.error('signIn error', error);
+        return false;
+      }
     },
   },
 };

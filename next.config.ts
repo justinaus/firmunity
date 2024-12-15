b6cd5b74cb95381e1464ef74fa990d44fa245e9d
justinaus <justinaus2k@gmail.com>
2024-12-15 10:47:43 +0900

    Update root page redirect - posts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/posts',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,50 +4,9 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Test</title>
+        <title>Hello</title>
       </Head>
-      <div
-        style={{
-          overscrollBehavior: 'none',
-        }}
-      >
-        Hello
-        <div
-          style={{
-            height: 500,
-          }}
-        >
-          111
-        </div>
-        <div
-          style={{
-            height: 500,
-          }}
-        >
-          222
-        </div>
-        <div
-          style={{
-            height: 500,
-          }}
-        >
-          333
-        </div>
-        <div
-          style={{
-            height: 500,
-          }}
-        >
-          444
-        </div>
-        <div
-          style={{
-            height: 500,
-          }}
-        >
-          555
-        </div>
-      </div>
+      <div>Hello</div>
     </>
   );
 }

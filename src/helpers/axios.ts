86447464a042dcedbf81e3b9c851bf2067a474d7
justinaus <justinaus2k@gmail.com>
2024-12-24 18:38:06 +0900

    Fix axios baseURL

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   // withCredentials: true, // 자격 증명 추가
-  // baseURL: process.env.NEXT_PUBLIC_API_DOMAIN,
+  baseURL:
+    process.env.NEXT_PUBLIC_API_DOMAIN || 'https://firmunity.vercel.app/',
 });
